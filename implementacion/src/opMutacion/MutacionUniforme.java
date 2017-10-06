d16a3c5c12543ee27e25103317a9b5eaacf329c8
StoneVerve <dimaorange@outlook.com>
2017-10-06 16:08:24 -0500

    Falta

--- implementacion/src/opMutacion/MutacionUniforme.java
+++ implementacion/src/opMutacion/MutacionUniforme.java
@@ -1,24 +1,28 @@
 import gaframework.*;
 import java.util.Random;
 
-public class MutacionUniforme<Interger> implements opMutacion<Integer> {
+public class MutacionUniforme implements MutationOp<Integer> {
 
 	private double probMutacion;
 
-	public MutacioUniforme(double probMut) {
+	public MutacionUniforme(double probMut) {
 		this.probMutacion = probMut;
 	}
 
 	public Genotype<Integer> mutate(Genotype<Integer> genotipo) {
-		return genotipo;		
-		/*int size = genotipo.size();
+		return genotipo;
+		int size = genotipo.size();
 		char gen;
 		Genotype<char> result = new Genotype<char>(size);
 		Random generator = new Random();
 		for (int i = 0; i < size; i++) {
-			
-			if (value <= probMutacion)
-		*/		
+			if (value <= probMutacion) {
+				gen = genotipo.getGene(i);
+				gen = auxMutate(gen);
+				result.setGene(i,gen);
+			} else {
+				result 
+				
 	}
 	
 	private Integer auxMutate(Integer gen) {
