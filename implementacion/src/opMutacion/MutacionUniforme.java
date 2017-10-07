8a0edb1020cca9b3113b62bbf4113f64a3752903
StoneVerve <dimaorange@outlook.com>
2017-10-07 02:10:46 -0500

    Operador de mutacion listo

--- implementacion/src/opMutacion/MutacionUniforme.java
+++ implementacion/src/opMutacion/MutacionUniforme.java
@@ -1,34 +1,52 @@
 import gaframework.*;
 import java.util.Random;
 
+/**
+ * Implemenatcion del operado de mutación uniforme para 
+ * maximizar una función
+ **/
 public class MutacionUniforme implements MutationOp<Integer> {
 
-	private double probMutacion;
+    private double probMutation;
 
-	public MutacionUniforme(double probMut) {
-		this.probMutacion = probMut;
-	}
+    public MutacionUniforme(double probMut) {
+        this.probMutation = probMut;
+    }
 
-	public Genotype<Integer> mutate(Genotype<Integer> genotipo) {
-		return genotipo;
-		int size = genotipo.size();
-		char gen;
-		Genotype<char> result = new Genotype<char>(size);
-		Random generator = new Random();
-		for (int i = 0; i < size; i++) {
-			if (value <= probMutacion) {
-				gen = genotipo.getGene(i);
-				gen = auxMutate(gen);
-				result.setGene(i,gen);
-			} else {
-				result 
-				
-	}
-	
-	private Integer auxMutate(Integer gen) {
-		if (gen.intValue() == 0)
-			return new Integer(1);
-		else
-			return new Integer(0);
-	}
+    /**
+     * Muta el genotipo dado utilizando el método de mutación uniforme utilizando
+     * una probabilidad de mutación descrita en el constructor
+     * @param genotyoe El genotipo a mutar
+     * @return El genotipo mutado
+     **/
+    public Genotype<Integer> mutate(Genotype<Integer> genotype) {
+        int size = genotype.size();
+        Integer gen;
+        Genotype<Integer> mutatedGeno = new Genotype<Integer>(size);
+        Random generator = new Random();
+        double dice; 
+        for (int i = 0; i < size; i++) {
+            dice = generator.nextDouble();
+            if (dice <= probMutation) {
+                gen = genotype.getGene(i);
+                gen = auxMutate(gen);
+                mutatedGeno.setGene(i,gen);
+            } else {
+                mutatedGeno.setGene(i,genotype.getGene(i));
+            }
+        }
+        return mutatedGeno;
+    }
+    
+    /**
+     * Función auxiliar que cambian el valor de un gen 
+     * @param gen El gen a mutar
+     * @return El gen mutado
+     **/
+    private Integer auxMutate(Integer gen) {
+        if (gen.intValue() == 0)
+            return new Integer(1);
+        else
+            return new Integer(0);
+    }
 }
